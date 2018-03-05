612e09f2a143beb0ca9671c6fcc1c0291d3cad20
tafitzs <tafitzs@hawaii.edu>
2018-03-05 13:05:50 -1000

    Complete AssignmentSeven

--- AssignmentSeven/ViewController.swift
+++ AssignmentSeven/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textField: UITextField!
+    
+    @IBOutlet var displayTextLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+ //1. Set the displayTextLabel text to a string with no characters
+        self.displayTextLabel.text=""
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //Create the IBAction
+    @IBAction func setLabelButtonPressed(_ sender: Any) {
+   //2. Create a local variable of type string to hold the user input text
+        var userText: String!
+        
+    //3. Set userText to the current text in the text field or user input text
+        userText = self.textField.text
+        
+    //4. Set the displayTextLabel text to the user input text
+        self.displayTextLabel.text=userText
+        
+    
+    }
+    
 }
 
